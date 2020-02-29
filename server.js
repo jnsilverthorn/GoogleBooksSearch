9288b4e72c79c1ee0053bb3577994b7ebcfe74aa
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2020-02-29 11:37:23 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/public"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mongoGoogleBooks",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
